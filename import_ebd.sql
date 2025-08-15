787a52a1dabf6c3430b97fa3dc0458077b9dd40b
Heidi Schlunt <heidiyah.15@gmail.com>
2025-08-15 09:36:02 -0700

    added .py script to break ebd into chunks

--- import_ebd.sql
+++ import_ebd.sql
@@ -1,1 +1,13 @@
-\COPY ebird FROM '\\HOMESTORAGE\Heidi\Data Analysis\ebird_data\ebd_relJun-2025\ebd_relJun-2025\one_thou_rows.txt' WITH (FORMAT text, DELIMITER E'\t', HEADER true); --sample file
+--sample file
+\COPY ebird FROM 'Z:\Data Analysis\ebird_data\ebd_relJun-2025\ebd_relJun-2025\one_thou_rows.txt' WITH (FORMAT text, DELIMITER E'\t', HEADER true, NULL '', ENCODING 'UTF8'); 
+
+--full file
+\COPY ebird FROM 'Z:\Data Analysis\ebird_data\ebd_relJun-2025\ebd_relJun-2025\ebd_relJun-2025.txt' WITH (FORMAT text, DELIMITER E'\t', HEADER true, NULL '', ENCODING 'UTF8'); 
+
+--truncate
+TRUNCATE TABLE ebird RESTART IDENTITY;
+
+--copy server-side
+COPY ebird
+FROM '/var/lib/postgresql/data/heidi/ebd_relJun-2025/ebd_relJun-2025.txt'
+WITH (FORMAT text, DELIMITER E'\t', HEADER true, NULL '', ENCODING 'UTF8');
