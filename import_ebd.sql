20e2321bae30ea3d1d7dfefd6ec1b944eacf27db
Heidi Schlunt <heidiyah.15@gmail.com>
2025-08-19 09:50:40 -0700

    added .sh to automate chunk input to postgres

--- import_ebd.sql
+++ import_ebd.sql
@@ -1,9 +1,3 @@
---sample file
-\COPY ebird FROM 'Z:\Data Analysis\ebird_data\ebd_relJun-2025\ebd_relJun-2025\one_thou_rows.txt' WITH (FORMAT text, DELIMITER E'\t', HEADER true, NULL '', ENCODING 'UTF8'); 
-
---full file
-\COPY ebird FROM 'Z:\Data Analysis\ebird_data\ebd_relJun-2025\ebd_relJun-2025\ebd_relJun-2025.txt' WITH (FORMAT text, DELIMITER E'\t', HEADER true, NULL '', ENCODING 'UTF8'); 
-
 --truncate
 TRUNCATE TABLE ebird RESTART IDENTITY;
 
