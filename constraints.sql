7a96e025d78d2bd643c49a4710976b503ad99e1e
Heidi Schlunt <heidiyah.15@gmail.com>
2025-09-29 15:38:07 -0700

    Updated files to reflect table name abbreviations, added date to pg_dump in linux_commands.txt, updated notes.txt

--- constraints.sql
+++ constraints.sql
@@ -1,19 +1,24 @@
 --Add primary keys
--- ALTER TABLE observation
+-- ALTER TABLE obs
 -- ADD PRIMARY KEY (global_unique_identifier);
 
--- ALTER TABLE submission
+-- ALTER TABLE sub
 -- ADD PRIMARY KEY (sampling_event_identifier);
 
--- ALTER TABLE location
+-- ALTER TABLE loc
 -- ADD PRIMARY KEY (locality_id);
 
--- ALTER TABLE taxon
+-- ALTER TABLE tax
 -- ADD PRIMARY KEY (common_name);
 
+-- ALTER TABLE sed
+-- ADD PRIMARY KEY (sampling_event_identifier);
+
 --Add foreign keys
-ALTER TABLE submission ADD CONSTRAINT sub_loc FOREIGN KEY (locality_id) REFERENCES location (locality_id);
+ALTER TABLE sub ADD CONSTRAINT sub_loc FOREIGN KEY (locality_id) REFERENCES location (locality_id);
 
-ALTER TABLE observation ADD CONSTRAINT obs_taxon FOREIGN KEY (common_name) REFERENCES taxon (common_name);
+ALTER TABLE obs ADD CONSTRAINT obs_taxon FOREIGN KEY (common_name) REFERENCES taxon (common_name);
 
-ALTER TABLE observation ADD CONSTRAINT obs_sub FOREIGN KEY (sampling_event_identifier) REFERENCES submission (sampling_event_identifier);
+ALTER TABLE obs ADD CONSTRAINT obs_sub FOREIGN KEY (sampling_event_identifier) REFERENCES sub (sampling_event_identifier);
+
+ALTER TABLE sed ADD CONSTRAINT sed_sub FOREIGN KEY (sampling_event_identifier) REFERENCES sub (sampling_event_identifier);
--- create_schema.sql
+++ create_schema.sql
@@ -1,63 +1,6 @@
---initial table import
-CREATE TABLE ebird (
-    GLOBAL_UNIQUE_IDENTIFIER text,
-    LAST_EDITED_DATE timestamp,
-    TAXONOMIC_ORDER text,
-    CATEGORY text,
-    TAXON_CONCEPT_ID text,
-    COMMON_NAME text,
-    SCIENTIFIC_NAME text,
-    SUBSPECIES_COMMON_NAME text,
-    SUBSPECIES_SCIENTIFIC_NAME text,
-    EXOTIC_CODE text,
-    OBSERVATION_COUNT text,
-    BREEDING_CODE text,
-    BREEDING_CATEGORY text,
-    BEHAVIOR_CODE text,
-    AGE_SEX text,
-    COUNTRY text,
-    COUNTRY_CODE text,
-    STATE text,
-    STATE_CODE text,
-    COUNTY text,
-    COUNTY_CODE text,
-    IBA_CODE text,
-    BCR_CODE text,
-    USFWS_CODE text,
-    ATLAS_BLOCK text,
-    LOCALITY text,
-    LOCALITY_ID text,
-    LOCALITY_TYPE text,
-    LATITUDE float,
-    LONGITUDE float,
-    OBSERVATION_DATE date,
-    TIME_OBSERVATIONS_STARTED time,
-    OBSERVER_ID text,
-    OBSERVER_ORCID_ID text,
-    SAMPLING_EVENT_IDENTIFIER text,
-    OBSERVATION_TYPE text,
-    PROTOCOL_NAME text,
-    PROTOCOL_CODE text,
-    PROJECT_NAMES text,
-    PROJECT_IDENTIFIERS text,
-    DURATION_MINUTES float,
-    EFFORT_DISTANCE_KM float,
-    EFFORT_AREA_HA float,
-    NUMBER_OBSERVERS float,
-    ALL_SPECIES_REPORTED int,
-    GROUP_IDENTIFIER text,
-    HAS_MEDIA int,
-    APPROVED int,
-    REVIEWED int,
-    REASON text,
-    CHECKLIST_COMMENTS text,
-    SPECIES_COMMENTS text
-)
+--NOTE: For faster INSERT assign keys after
 
---creating orthogonal tables
---NOTE: For faster INSERT remove keys and assign after
-
-CREATE TABLE taxon (
+CREATE TABLE tax (
     TAXONOMIC_ORDER int,
     TAXON_CONCEPT_ID text,
     CATEGORY text,
@@ -67,7 +10,7 @@ CREATE TABLE taxon (
     SUBSPECIES_SCIENTIFIC_NAME text
 );
 
-CREATE TABLE observation (
+CREATE TABLE obs (
     GLOBAL_UNIQUE_IDENTIFIER text UNIQUE NOT NULL PRIMARY KEY,
     LAST_EDITED_DATE timestamp,
     COMMON_NAME text REFERENCES taxon(COMMON_NAME),
@@ -82,7 +25,7 @@ CREATE TABLE observation (
     SPECIES_COMMENTS text
 );
 
-CREATE TABLE submission (
+CREATE TABLE sub (
     LOCALITY_ID text REFERENCES location(LOCALITY_ID),
     OBSERVATION_DATE date,
     TIME_OBSERVATIONS_STARTED time,
@@ -106,7 +49,7 @@ CREATE TABLE submission (
     REASON text
 );
 
-CREATE TABLE location (
+CREATE TABLE loc (
     COUNTRY text,
     COUNTRY_CODE text,
     STATE text,
